8ece2462eda4b6df4e7dcc1663c8ffa7102e1b69
sharonSofia <sharonsanta1626@gmail.com>
2023-11-23 14:50:04 -0800

    test-in-js

---
+++ index.js
@@ -0,0 +1,4 @@
+const displayVrAnterior= document.getElementById('valor-anterior');
+const displayVrActual= document.getElementById('valor-actual');
+const botonesNumeros= document.querySelectorAll('.numero');
+const botonesOperadores= document.querySelectorAll('.operador');
